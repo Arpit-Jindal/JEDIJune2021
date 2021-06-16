62fd6b79a6f2b9fd082e4eb4760ec463f78963f7
arpit <arpitjindal3@gmail.com>
2021-06-16 15:43:31 +0530

    Below changes: 	new file:   .gitignore 	new file:   Code/hello.java 	modified:   Code/helloWorld.c

---
+++ Code/hello.java
@@ -0,0 +1,9 @@
+//sample java cod
+// A Java program to print "Hello World" 
+import java.*;
+public class Main { 
+    public static void main(String args[]) 
+    { 
+        System.out.println("Hello World"); 
+    } 
+}
